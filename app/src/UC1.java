d43f91762e23d75b912496028d925d04bd6ef637
akilcn01-oss <ac4996@srmist.edu.in>
2026-03-20 09:54:36 +0530

    akil added UC1

---
+++ app/src/UC1.java
@@ -0,0 +1,7 @@
+public class
+UseCase1HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("   Welcome to Hotel Booking management System");
+        System.out.println("   system intialized successfully. ");
+    }
+}
